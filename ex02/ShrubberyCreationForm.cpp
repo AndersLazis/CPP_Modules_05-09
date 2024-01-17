e9fc984540cba8859c36b4b0982f89693aa3fb7e
Andrey Putiev <aputiev@c2r13s8.42wolfsburg.de>
2024-01-17 18:30:35 +0100

    updated

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -13,38 +13,31 @@
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include "includes/ShrubberyCreationForm.hpp"
 #include "includes/Colors.hpp"
+#include "includes/AForm.hpp"
+
   
 
 /*  ================*= Constructors =*================= */
 /* Default */
-ShrubberyCreationForm::ShrubberyCreationForm() : _formName("default ShrubberyCreationForm"),                
-                _gradeRequiredToSign(145),
-                _gradeRequiredToExecute(137),
-                _isSigned(false),
-                _target("default target")
+ShrubberyCreationForm::ShrubberyCreationForm() : AForm("default ShrubberyCreationForm", 145, 137),
+                                                _target("default target")
 {
-    std::cout << BG_YELLOW << "ShrubberyCreationForm \"" << _formName << "\" with grade required to sign" << 
+    std::cout << BG_YELLOW << "ShrubberyCreationForm \"" << getFormName() << "with grade required to sign" << 
         _gradeRequiredToSign << " and grade require to execute " << _gradeRequiredToExecute << " was created" << RESET << 
         " and target " << _target << std::endl << RESET;
 }
 /* Constructor with parameters */
-ShrubberyCreationForm::ShrubberyCreationForm(std::string target) :    _formName("Shrubbery form"),                                                                                
-                                                                        _gradeRequiredToSign(145),
-                                                                        _gradeRequiredToExecute(137),
-                                                                        _isSigned(false),
-                                                                        _target(target)
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target) : AForm("default ShrubberyCreationForm", 145, 137),
+                                                                    _target(target)
 {
-    std::cout << BG_YELLOW << "ShrubberyCreationForm \"" << _formName << "\" with grade required to sign" << 
+    std::cout << BG_YELLOW << "ShrubberyCreationForm \"" <<  getFormName() << "\" with grade required to sign" << 
         _gradeRequiredToSign << " and grade require to execute " << _gradeRequiredToExecute << " was created"  << RESET << std::endl << RESET;
 }
 /* Copy constructor */
-ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const & source) : _formName(source._formName),
-                                    _gradeRequiredToSign(source._gradeRequiredToSign),
-                                    _gradeRequiredToExecute(source._gradeRequiredToExecute),
-                                    _isSigned(false),
-                                    _target(source._target)
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const & source) : AForm(source), _target(source._target)
 {
     std::cout << BG_YELLOW << "ShrubberyCreationForm copy constructor was called"  << RESET << std::endl << RESET;
    return;
@@ -62,7 +55,7 @@ ShrubberyCreationForm::~ShrubberyCreationForm()
 ShrubberyCreationForm & ShrubberyCreationForm::operator=(ShrubberyCreationForm const & source)
 {
     std::cout << BG_YELLOW << "ShrubberyCreationForm \"=\" operator overload was called"  << RESET << std::endl << RESET;
-    this->_isSigned = source._isSigned;
+    this->_target = source._target;
     return *this;
 }
 
@@ -70,75 +63,27 @@ ShrubberyCreationForm & ShrubberyCreationForm::operator=(ShrubberyCreationForm c
 
 std::ostream& operator<<(std::ostream& os, const ShrubberyCreationForm& ShrubberyCreationForm)
 {
-    os << RESET << BG_GREEN << "ShrubberyCreationForm name: " << ShrubberyCreationForm.getShrubberyCreationFormName() << 
+    os << RESET << BG_GREEN << "ShrubberyCreationForm name: " << ShrubberyCreationForm.getFormName() << 
                         " ShrubberyCreationForm is signed: " << ShrubberyCreationForm.getIsSigned() <<
                         " ShrubberyCreationForm grade required to sign: " << ShrubberyCreationForm.getGradeRequiredToSign() << 
                         " ShrubberyCreationForm grade required to execute: "<< ShrubberyCreationForm.getGradeRequiredToExecute() << 
-                        " ShrubberyCreationForm target: " << ShrubberyCreationForm._target << RESET << std::endl << RESET;
+                        " ShrubberyCreationForm target: " << ShrubberyCreationForm.getTarget() << RESET << std::endl << RESET;
     return os;
 }
 
 /*  =================*=  Methods: =*================= */
 
-std::string const ShrubberyCreationForm::getShrubberyCreationFormName() const
+ std::string const & ShrubberyCreationForm::getTarget() const
 {
-    return this->_formName;
+    return this->_target;
 }
 
-bool ShrubberyCreationForm::getIsSigned() const
-{
-    return this->_isSigned;
-}
 
-int ShrubberyCreationForm::getGradeRequiredToSign() const
-{
-    return this->_gradeRequiredToSign;
-}
-
-int ShrubberyCreationForm::getGradeRequiredToExecute() const
-{
-    return this->_gradeRequiredToExecute;
-}
-
-const char* ShrubberyCreationForm::GradeTooLowException::what()const throw()
-{
-    return "Error: Grade is too low";
-}
-
-const char* ShrubberyCreationForm::GradeTooHighException::what()const throw()
-{
-    return "Error: Grade is too high";
-}
-
-const char* ShrubberyCreationForm::ShrubberyCreationFormAlreadySignedException::what()const throw()
-{
-    return "Error: Form is already signed!";
-}
-
-const char* ShrubberyCreationForm::ShrubberyCreationFormANotSignedException::what()const throw()
-{
-    return "Error: Form is not signed yet !";
-}
-
-void ShrubberyCreationForm::beSigned(Bureaucrat const & bureaucrat)
-{
-    if (bureaucrat.getGrade() > this->_gradeRequiredToSign)
-        throw ShrubberyCreationForm::GradeTooLowException();
-    else if (this->_isSigned)
-        throw ShrubberyCreationForm::ShrubberyCreationFormAlreadySignedException();       
-    else
-       this->_isSigned = true;
-    return;
-}
-
-void ShrubberyCreationForm::execute(Bureaucrat const & bureaucrat) const
+void ShrubberyCreationForm::beExecuted() const
 {   
-   
-    if (bureaucrat.getGrade() > this->_gradeRequiredToExecute)
-        throw ShrubberyCreationForm::GradeTooLowException();
-    else if (!this->_isSigned)
-        throw ShrubberyCreationForm::ShrubberyCreationFormNotSignedException();
-    else
+    std::ofstream out;
+    std::string filename = this->_target + "shrubbery";
+    if(out.open(filename.c_str()))       
     {
         std::string filename = this->_target + "_shrubbery";
         std::ofstream outputFile(filename.c_str(), std::ios::app);
